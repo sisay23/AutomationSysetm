31c12d50a40139415df4a68782556e8c46aa885f
Nabil Seid <nabeelseid@gmail.com>
2021-07-31 19:21:55 +0300

    setup database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-require("dotenv").config({ path: "./config.env" });
-
+require("dotenv").config({ path: __dirname +  "/../config.env" });
+console.log(process.env.ATLAS_URI)
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require("./routes/record.ts"));
 // get driver connection
 
-const dbo = require("./db/conn");
+const dbo = require("./db/conn.ts");
  
 app.listen(port, () => {
   // perform a database connection when server starts
